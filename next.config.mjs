ed1f4ce5eb1354bff3a52d29bc65eb09e9621886
Ehren Nwokocha <nwokochaehren@gmail.com>
2023-04-13 19:29:42 +0100

    Ignore Build Errors on Vercel swc minify

--- next.config.mjs
+++ next.config.mjs
@@ -21,5 +21,12 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true
 };
 export default config;
