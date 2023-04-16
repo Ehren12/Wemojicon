05cd7ac094f3b8e61c4ae3a9a09ef16bb2fa6e43
Ehren Nwokocha <nwokochaehren@gmail.com>
2023-04-16 12:44:39 +0100

    Nested links and customized scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 import { type Config } from "tailwindcss";
+import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindScrollbar({ nocompatible: true })],
 } satisfies Config;
